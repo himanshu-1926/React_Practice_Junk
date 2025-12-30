0a204539e994366389a0fd320a5f174fdbf7d648
Himanshu Yadav <iamhimanshu1926@gmail.com>
2025-12-30 18:27:38 +0530

    junk practice

---
+++ code6_css/src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './header.module.css'
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h1>hii hmnsh</h1>
+      <button className='btn'>login</button>
+    </div>
+  )
+}
+
+export default Header
